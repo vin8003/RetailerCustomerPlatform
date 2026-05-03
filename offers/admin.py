42dfd4fbc9cd083c2e39a8f943e6a36cd237f719
vin8003 <vin8003@gmail.com>
2026-05-03 21:13:37 +0530

    Fix field name errors in Django admin registrations

--- offers/admin.py
+++ offers/admin.py
@@ -3,14 +3,14 @@ from .models import Offer, OfferTarget, OfferRedemption
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ['name', 'retailer', 'offer_type', 'discount_type', 'discount_value', 'is_active', 'start_date', 'end_date']
-    list_filter = ['offer_type', 'discount_type', 'is_active', 'start_date', 'end_date']
+    list_display = ['name', 'retailer', 'offer_type', 'value_type', 'value', 'is_active', 'start_date', 'end_date']
+    list_filter = ['offer_type', 'value_type', 'is_active', 'start_date', 'end_date']
     search_fields = ['name', 'description', 'retailer__shop_name']
     ordering = ['-created_at']
 
 @admin.register(OfferTarget)
 class OfferTargetAdmin(admin.ModelAdmin):
-    list_display = ['offer', 'target_type', 'target_id']
+    list_display = ['offer', 'target_type']
     list_filter = ['target_type']
     search_fields = ['offer__name']
 
